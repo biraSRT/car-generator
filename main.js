ee2d0c75b50a221b28e9067dd03157dfbc172631
biraSRT <95512405+biraSRT@users.noreply.github.com>
2021-12-05 21:20:46 -0500

    Changed spacing

--- main.js
+++ main.js
@@ -3,9 +3,7 @@
 //Initializing string arrays for random messages
 
 const sportCars = ['Mazda Miata' , 'Toyota Supra', 'Chevrolet Corvette', 'Porsche 911', 'Audi RS7', 'BMW M4', 'Ford Mustang', 'Mercedes-AMG GT', 'Ferrari F8'];
-
 const carColors = ['Black', 'White', 'Grey', 'Silver', 'Blue', 'Red', 'Brown', 'Green', 'Gold'];
-
 const carYear = ['2016', '2017', '2018', '2019', '2020', '2021', '2022'];
 
 
