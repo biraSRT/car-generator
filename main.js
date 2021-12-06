0ee7125b66e4dd3247d6f7a26cafd53d107e081e
biraSRT <95512405+biraSRT@users.noreply.github.com>
2021-12-05 21:42:52 -0500

    reworked whole script added object, for loop and switch case

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 //Made by BiraSRT
-
-//Initializing string arrays for random messages
-
-const sportCars = ['Mazda Miata' , 'Toyota Supra', 'Chevrolet Corvette', 'Porsche 911', 'Audi RS7', 'BMW M4', 'Ford Mustang', 'Mercedes-AMG GT', 'Ferrari F8'];
-const carColors = ['Black', 'White', 'Grey', 'Silver', 'Blue', 'Red', 'Brown', 'Green', 'Gold'];
-const carYear = ['2016', '2017', '2018', '2019', '2020', '2021', '2022'];
-
+//Car maker object holding car model, colors and year
+const sportCarMaker = {
+    sportCars: ['Mazda Miata' , 'Toyota Supra', 'Chevrolet Corvette', 'Porsche 911', 'Audi RS7', 'BMW M4', 'Ford Mustang', 'Mercedes-AMG GT', 'Ferrari F8'],
+    carColors: ['Black', 'White', 'Grey', 'Silver', 'Blue', 'Red', 'Brown', 'Green', 'Gold'],
+    carYear: ['2016', '2017', '2018', '2019', '2020', '2021', '2022']
+};
 
 // function that randomizes  message
 function randomize (arr) {
@@ -15,11 +14,30 @@ function randomize (arr) {
     return random;
 }
 
-let randomSportCar = sportCars[randomize (sportCars)];
-let randomCarColor = carColors[randomize (carColors)];
-let randomCarYear = carYear[randomize (carYear)];
+//Making for loop and switch case to loop throught object and get random value for each propertie
+let randomSportCar;
+let randomCarColor;
+let randomCarYear;
 
-//Output random sports car / message
+for (let key in sportCarMaker) {
+
+    let randomIndex = randomize(sportCarMaker[key])
+
+    switch (key) {
+
+        case 'sportCars':
+            randomSportCar = sportCarMaker[key][randomIndex];
+            break;
+        case 'carColors':
+            randomCarColor = sportCarMaker[key][randomIndex];
+            break;
+        case 'carYear':
+            randomCarYear = sportCarMaker[key][randomIndex];
+            break;
+    }
+};
+
+//Outputs random sports car
 console.log(`Your random sports car is a ${randomCarColor} ${randomSportCar} ${randomCarYear}!`)
 
 
